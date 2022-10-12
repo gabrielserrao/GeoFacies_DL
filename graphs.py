799f622285a12940bfaa327c677d663ffa677d2f
gabriel <gabriel.serrao.seabra@gmail.com>
2022-10-12 10:48:56 +0200

    change

---
+++ graphs.py
@@ -0,0 +1,26 @@
+#%%
+#find ninimum value element of two numpy arrays
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def find_min(a,b):
+    c = np.minimum(a,b)
+    return c
+
+
+
+
+def main():
+    a = np.array([1,2,3,4,5])
+    b = np.array([5,4,3,2,1])
+    print(find_min(a,b))
+
+
+if __name__ == "__main__":
+    main()
+
+# %%
